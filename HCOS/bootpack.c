bd3afcaf637fcc9ec573cd72701e4e5076a7914d
HuangCheng72 <huangcheng20000702@gmail.com>
2022-06-26 13:09:16 +0800

    Day04 08 completed.

--- HCOS/bootpack.c
+++ HCOS/bootpack.c
@@ -1,22 +1,116 @@
-//函数声明
 void io_hlt(void);
-void write_mem8(int addr,int data);
+void io_cli(void);
+void io_out8(int port, int data);
+int io_load_eflags(void);
+void io_store_eflags(int eflags);
+
+//下面是我们这次需要用到的函数 
+
+void init_palette(void); //初始化调色板 
+void set_palette(int start, int end, unsigned char *rgb); //设置调色板颜色 
+void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1); //对某一区域染色也就是绘制矩形，xsize是屏幕横轴的大小 
+
+//下面是颜色常量 
+
+#define COL8_000000		0
+#define COL8_FF0000		1
+#define COL8_00FF00		2
+#define COL8_FFFF00		3
+#define COL8_0000FF		4
+#define COL8_FF00FF		5
+#define COL8_00FFFF		6
+#define COL8_FFFFFF		7
+#define COL8_C6C6C6		8
+#define COL8_840000		9
+#define COL8_008400		10
+#define COL8_848400		11
+#define COL8_000084		12
+#define COL8_840084		13
+#define COL8_008484		14
+#define COL8_848484		15
 
 void HariMain(void){
-    
-    int i;
-    char *p; //因为在汇编里面我们指定的地址指向的内存空间是BYTE型，对应C语言中就是char型地址 
-    //绘制条纹图案 
-    for(i = 0xa0000; i <= 0xaffff; i++){
-        //write_mem8(i, i & 0x0f);
-        
-        p = (char*)i; //直接将i转化为char型指针
-		*p = i & 0x0f; //直接对这个地址进行写入数据 
-    }
-    
-    //这个就是说一直死循环然后又中断，和之前中断之后又goto fin一样的效果
-    while(1){
-        io_hlt();
-    }
+	//染色区域指针 
+	char *vram;
+	int xsize, ysize;
+
+	init_palette();
+	vram = (char *) 0xa0000;
+	xsize = 320;
+	ysize = 200;
+	
+	//以下就是绘图过程 
+
+	boxfill8(vram, xsize, COL8_008484,  0,         0,          xsize -  1, ysize - 29);
+	boxfill8(vram, xsize, COL8_C6C6C6,  0,         ysize - 28, xsize -  1, ysize - 28);
+	boxfill8(vram, xsize, COL8_FFFFFF,  0,         ysize - 27, xsize -  1, ysize - 27);
+	boxfill8(vram, xsize, COL8_C6C6C6,  0,         ysize - 26, xsize -  1, ysize -  1);
+
+	boxfill8(vram, xsize, COL8_FFFFFF,  3,         ysize - 24, 59,         ysize - 24);
+	boxfill8(vram, xsize, COL8_FFFFFF,  2,         ysize - 24,  2,         ysize -  4);
+	boxfill8(vram, xsize, COL8_848484,  3,         ysize -  4, 59,         ysize -  4);
+	boxfill8(vram, xsize, COL8_848484, 59,         ysize - 23, 59,         ysize -  5);
+	boxfill8(vram, xsize, COL8_000000,  2,         ysize -  3, 59,         ysize -  3);
+	boxfill8(vram, xsize, COL8_000000, 60,         ysize - 24, 60,         ysize -  3);
+
+	boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 24, xsize -  4, ysize - 24);
+	boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 23, xsize - 47, ysize -  4);
+	boxfill8(vram, xsize, COL8_FFFFFF, xsize - 47, ysize -  3, xsize -  4, ysize -  3);
+	boxfill8(vram, xsize, COL8_FFFFFF, xsize -  3, ysize - 24, xsize -  3, ysize -  3);
+	
+	//循环中断，防止退出 
+
+	while(1) {
+		io_hlt();
+	}
 }
 
+void init_palette(void){
+	static unsigned char table_rgb[16 * 3] = {
+		0x00, 0x00, 0x00,	/*  0:黑 */
+		0xff, 0x00, 0x00,	/*  1:亮红 */
+		0x00, 0xff, 0x00,	/*  2:亮绿 */
+		0xff, 0xff, 0x00,	/*  3:亮黄 */
+		0x00, 0x00, 0xff,	/*  4:亮蓝 */
+		0xff, 0x00, 0xff,	/*  5:亮紫 */
+		0x00, 0xff, 0xff,	/*  6:浅亮蓝 */
+		0xff, 0xff, 0xff,	/*  7:白 */
+		0xc6, 0xc6, 0xc6,	/*  8:亮灰 */
+		0x84, 0x00, 0x00,	/*  9:暗红 */
+		0x00, 0x84, 0x00,	/* 10:暗绿 */
+		0x84, 0x84, 0x00,	/* 11:暗黄 */
+		0x00, 0x00, 0x84,	/* 12:暗青 */
+		0x84, 0x00, 0x84,	/* 13:暗紫色 */
+		0x00, 0x84, 0x84,	/* 14:浅暗蓝 */
+		0x84, 0x84, 0x84	/* 15:暗灰 */
+	};
+	set_palette(0, 15, table_rgb);
+	return;
+
+	/* C语言中的static char只能用于数据，相当于汇编中的DB */
+}
+
+void set_palette(int start, int end, unsigned char *rgb){
+	int i, eflags;
+	eflags = io_load_eflags();	/* 记录中断许可标志的值 */
+	io_cli(); 					/* 将许可标志设置为0，也就是禁止中断 */
+	io_out8(0x03c8, start);
+	for (i = start; i <= end; i++) {
+		io_out8(0x03c9, rgb[0] / 4);
+		io_out8(0x03c9, rgb[1] / 4);
+		io_out8(0x03c9, rgb[2] / 4);
+		rgb += 3;
+	}
+	io_store_eflags(eflags);	/* 恢复中断许可标志的值 */
+	return;
+}
+
+void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1){
+	int x, y;
+	for (y = y0; y <= y1; y++) {
+		for (x = x0; x <= x1; x++){
+			vram[y * xsize + x] = c;
+		}
+	}
+	return;
+}
--- HCOS/naskfunc.nas
+++ HCOS/naskfunc.nas
@@ -10,7 +10,11 @@
 
 [FILE "naskfunc.nas"]			; 源文件名信息
 
-		GLOBAL	_io_hlt,_write_mem8			; 暴露程序中包含的函数名
+		; GLOBAL	_io_hlt,_write_mem8			; 暴露程序中包含的函数名
+        GLOBAL	_io_hlt, _io_cli, _io_sti, _io_stihlt
+		GLOBAL	_io_in8,  _io_in16,  _io_in32
+		GLOBAL	_io_out8, _io_out16, _io_out32
+		GLOBAL	_io_load_eflags, _io_store_eflags
 
 
 ; 以下是实际的函数
@@ -20,8 +24,62 @@
 _io_hlt:	; void io_hlt(void);
 		HLT
 		RET
-_write_mem8:	; void write_mem8(int addr, int data); 这里的8不知道是什么意思，一个int的长度是4，这里大概就是直接在函数的内存空间里面取参数，然后进行操作，以此类推，第一个参数是[ESP + 4]，第二个参数是[ESP + 8]，第三个参数是[ESP + 12]，如此下去具体的值自己算
-        MOV		ECX,[ESP+4]     ; [ESP + 4]中存放的是地址，将这个地址读入ECX寄存器
-        MOV		AL,[ESP+8]      ; [ESP + 8]中存放的是数据，将其读入AL寄存器
-        MOV     [ECX],AL        ; 将AL的数据写入到ECX所在的内存空间
-        RET
+
+_io_cli:	; void io_cli(void);
+		CLI
+		RET
+
+_io_sti:	; void io_sti(void);
+		STI
+		RET
+
+_io_stihlt:	; void io_stihlt(void);
+		STI
+		HLT
+		RET
+
+_io_in8:	; int io_in8(int port);
+		MOV		EDX,[ESP+4]		; port
+		MOV		EAX,0
+		IN		AL,DX
+		RET
+
+_io_in16:	; int io_in16(int port);
+		MOV		EDX,[ESP+4]		; port
+		MOV		EAX,0
+		IN		AX,DX
+		RET
+
+_io_in32:	; int io_in32(int port);
+		MOV		EDX,[ESP+4]		; port
+		IN		EAX,DX
+		RET
+
+_io_out8:	; void io_out8(int port, int data);
+		MOV		EDX,[ESP+4]		; port
+		MOV		AL,[ESP+8]		; data
+		OUT		DX,AL
+		RET
+
+_io_out16:	; void io_out16(int port, int data);
+		MOV		EDX,[ESP+4]		; port
+		MOV		EAX,[ESP+8]		; data
+		OUT		DX,AX
+		RET
+
+_io_out32:	; void io_out32(int port, int data);
+		MOV		EDX,[ESP+4]		; port
+		MOV		EAX,[ESP+8]		; data
+		OUT		DX,EAX
+		RET
+
+_io_load_eflags:	; int io_load_eflags(void);
+		PUSHFD		; 这里是PUSH EFLAGS，也就是压栈
+		POP		EAX ; 弹栈，把EFLAGS弹到EAX寄存器里面
+		RET
+
+_io_store_eflags:	; void io_store_eflags(int eflags);
+		MOV		EAX,[ESP+4]
+		PUSH	EAX
+		POPFD		; 这里是POP EFLAGS，也就是弹栈，与load的功能相反，把EAX的值弹到EFLAGS里面
+		RET
